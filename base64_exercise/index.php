82e75718a6bb0f2ea990f653f0081781a90d9a4c
Cedric Priestley <cpriestley@dminc.com>
2016-08-20 22:08:52 -0400

    Comment test code

--- base64_exercise/index.php
+++ base64_exercise/index.php
@@ -4,7 +4,7 @@ $url = "https://raw.githubusercontent.com/fourkitchens/careers/master/exercises/
 $html = file_get_contents($url);
 convert_src_tags($html);
 file_put_contents("index.html", $html);
-print $html;
+//print $html;
 
 // Convert image sources to base64 data URIs
 function convert_src_tags(&$html) {
